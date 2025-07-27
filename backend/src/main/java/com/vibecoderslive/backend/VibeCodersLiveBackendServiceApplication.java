d2632bf57b42486b3e44b9fb51b0b4e0a37fc229
RootGeorge17 <george.sopov17@gmail.com>
2025-07-27 11:54:34 +0100

    Initial backend code

---
+++ backend/src/main/java/com/vibecoderslive/backend/VibeCodersLiveBackendServiceApplication.java
@@ -0,0 +1,14 @@
+package com.vibecoderslive.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeCodersLiveBackendServiceApplication {
+
+	public static void main(String[] args) {
+		System.out.println("null");
+		SpringApplication.run(VibeCodersLiveBackendServiceApplication.class, args);
+	}
+
+}
